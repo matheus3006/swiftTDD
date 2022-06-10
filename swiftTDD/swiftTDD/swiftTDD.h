74fdcf3c1dfbba661dd20235088526c25b28ffd8
matheus3006 <matheussouza18@live.com>
2022-06-09 21:06:58 -0300

    chore: add domain layer

---
+++ swiftTDD/swiftTDD/swiftTDD.h
@@ -0,0 +1,18 @@
+//
+//  swiftTDD.h
+//  swiftTDD
+//
+//  Created by Matheus Souza on 6/9/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for swiftTDD.
+FOUNDATION_EXPORT double swiftTDDVersionNumber;
+
+//! Project version string for swiftTDD.
+FOUNDATION_EXPORT const unsigned char swiftTDDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftTDD/PublicHeader.h>
+
+
