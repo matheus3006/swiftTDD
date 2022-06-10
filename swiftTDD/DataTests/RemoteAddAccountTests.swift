afae110431f443b10054f86c8dd01d318100246d
matheus3006 <matheussouza18@live.com>
2022-06-10 14:26:07 -0300

    feat: add Model protocol with toData Extension

--- swiftTDD/DataTests/RemoteAddAccountTests.swift
+++ swiftTDD/DataTests/RemoteAddAccountTests.swift
@@ -11,8 +11,8 @@ class RemoteAddAccount {
     }
     
     func add(addAccountModel: AddAccountModel){
-        let data = try? JSONEncoder().encode(addAccountModel)
-        httpClient.post(to: url, with: data)
+        
+        httpClient.post(to: url, with: addAccountModel.toData())
     }
 }
 
@@ -40,11 +40,9 @@ class RemoteAddAccountTests: XCTestCase {
         let (sut,httpClientSpy) = makeSut()
         let addAccountModel = makeAddAccountModel()
         
-        
         sut.add(addAccountModel: addAccountModel)
-        let data = try? JSONEncoder().encode(addAccountModel)
-        
-        XCTAssertEqual(httpClientSpy.data, data)
+     
+        XCTAssertEqual(httpClientSpy.data, addAccountModel.toData())
         
     }
     
