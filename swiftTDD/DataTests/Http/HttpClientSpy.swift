4b92eeb3a83cacb5b1ba8f8ffa3d388335ecae8b
matheus3006 <matheussouza18@live.com>
2022-06-20 15:43:38 -0300

    feat: add randomize and thread sanitiser on data layer

--- swiftTDD/DataTests/Http/HttpClientSpy.swift
+++ swiftTDD/DataTests/Http/HttpClientSpy.swift
@@ -4,16 +4,15 @@ import Data
 
 
 class HttpClientSpy: HttpPostClient {
-    var urls : [URL] = []
-    var data : Data?
-    var completion:((Result<Data,HttpError>) -> Void)?
+    var urls = [URL]()
+     var data: Data?
+     var completion: ((Result<Data?, HttpError>) -> Void)?
 
-    
-    func post(to url: URL,with data: Data?, completion: @escaping (Result<Data?, HttpError> ) -> Void) {
-        self.urls.append(url)
-        self.data = data
-        self.completion = completion
-    }
+     func post(to url: URL, with data: Data?, completion: @escaping (Result<Data?, HttpError>) -> Void) {
+         self.urls.append(url)
+         self.data = data
+         self.completion = completion
+     }
     
     func completeWIthError(_ error: HttpError){
         completion?(.failure(error))
