43bbb134a53c17034cf0b1e70c9201a7c76d6b74
matheus3006 <matheussouza18@live.com>
2022-06-25 20:59:42 -0300

    refactor: move presentation tests to their own files

--- swiftTDD/PresentationTests/Presenters/SignUpPresenterTests.swift
+++ swiftTDD/PresentationTests/Presenters/SignUpPresenterTests.swift
@@ -1,64 +1,6 @@
 
-class SignUpPresenter{
-    private let alertView: AlertView
-    
-    init(alertView:AlertView){
-        self.alertView=alertView
-    }
-    
-    func signUp(viewModel: SignUpViewModel){
-        if let message = validate(viewModel: viewModel){
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
-        }
-        
-    }
-    private func validate(viewModel: SignUpViewModel) -> String? {
-        if viewModel.name == nil || viewModel.name!.isEmpty {
-            return "O campo nome é obrigatório"
-            
-        }
-        
-        if viewModel.email == nil || viewModel.email!.isEmpty {
-           return "O campo email é obrigatório"
-            
-        }
-        
-        
-        if viewModel.password == nil || viewModel.password!.isEmpty {
-            return "O campo senha é obrigatório"
-            
-        }
-        
-        
-        if viewModel.passwordConfirmation == nil || viewModel.passwordConfirmation!.isEmpty {
-          return "O campo confirmar senha é obrigatório"
-            
-        }
-        return nil
-    }
-    
-}
-
-protocol AlertView{
-    func showMessage(viewModel:AlertViewModel)
-    
-}
-
-struct AlertViewModel: Equatable
-{
-    var title:String
-    var message:String
-}
-
-struct SignUpViewModel{
-    var name:String?
-    var email:String?
-    var password:String?
-    var passwordConfirmation:String?
-    
-}
-
 import XCTest
+import Presentation
 
 class SignUpPresenterTests: XCTestCase {
     func test_signUp_should_show_error_message_if_name_is_not_provided() throws {
