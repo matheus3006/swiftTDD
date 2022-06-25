0907450411986a3f59e7f639632f616b2db07399
matheus3006 <matheussouza18@live.com>
2022-06-25 19:54:23 -0300

    feat: ensure SignUpPresenter shows error message if no name is provided

--- swiftTDD/PresentationTests/SignUpPresenterTests.swift
+++ swiftTDD/PresentationTests/SignUpPresenterTests.swift
@@ -1,18 +1,64 @@
-//
-//  PresentationTests.swift
-//  PresentationTests
-//
-//  Created by Matheus Souza on 6/25/22.
-//
+
+class SignUpPresenter{
+    private let alertView: AlertView
+    
+    init(alertView:AlertView){
+        self.alertView=alertView
+    }
+    
+    func signUp(viewModel: SignUpViewModel){
+        if viewModel.name == nil || viewModel.name!.isEmpty {
+            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "O campo nome é obrigatório"))
+            
+        }
+    }
+}
+
+protocol AlertView{
+    func showMessage(viewModel:AlertViewModel)
+    
+}
+
+struct AlertViewModel: Equatable
+{
+    var title:String
+    var message:String
+}
+
+struct SignUpViewModel{
+    var name:String?
+    var email:String?
+    var password:String?
+    var passwordConfirmation:String?
+    
+}
 
 import XCTest
-@testable import Presentation
 
 class SignUpPresenterTests: XCTestCase {
-
-
-    func test_() throws {
-      
+    
+    
+    func test_signUp_should_show_error_message_if_name_is_not_provided() throws {
+        let alertViewSpy = AlertViewSpy()
+        let sut = SignUpPresenter(alertView: alertViewSpy)
+        let signUpViewModel = SignUpViewModel(email: "any_email@mail.com",password:"any_password",passwordConfirmation:"any_password")
+        
+        sut.signUp(viewModel: signUpViewModel)
+        
+        XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title:"Falha na validação", message: "O campo nome é obrigatório"))
     }
+    
+}
 
+
+extension SignUpPresenterTests{
+    class AlertViewSpy: AlertView{
+        var viewModel: AlertViewModel?
+        
+        func showMessage(viewModel: AlertViewModel) {
+            self.viewModel = viewModel
+        }
+    }
+    
+    
 }
