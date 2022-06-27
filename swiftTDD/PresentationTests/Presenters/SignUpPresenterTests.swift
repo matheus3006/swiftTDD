19bd442ca2d94424a75582eb0c17d0c678be0d6f
matheus3006 <matheussouza18@live.com>
2022-06-27 19:14:28 -0300

    feat: change LoadingViewSpy to use an observer

--- swiftTDD/PresentationTests/Presenters/SignUpPresenterTests.swift
+++ swiftTDD/PresentationTests/Presenters/SignUpPresenterTests.swift
@@ -163,14 +163,20 @@ class SignUpPresenterTests: XCTestCase {
     func test_signUp_should_show_loading_before_call_addAccount() throws {
         let loadingViewSpy = LoadingViewSpy()
         let sut=makeSut(loadingView:loadingViewSpy)
+        let exp = expectation(description: "waiting")
         
+        loadingViewSpy.observe { viewModel in
+            XCTAssertEqual(viewModel,LoadingViewModel(isLoading:true))
+            exp.fulfill()
+        }
         sut.signUp(viewModel:makeSignUpViewModel())
 
-        XCTAssertEqual(loadingViewSpy.viewModel,LoadingViewModel(isLoading:true))
+        wait(for: [exp], timeout: 1)
 
     }
     
     
+
 }
 
 
@@ -237,10 +243,14 @@ extension SignUpPresenterTests{
     }
     
     class LoadingViewSpy: LoadingView {
-        var viewModel: LoadingViewModel?
+        var emit: ((LoadingViewModel) -> Void)?
+        
+        func observe(completion: @escaping (LoadingViewModel) -> Void){
+            self.emit = completion
+        }
         
         func display(viewModel: LoadingViewModel) {
-            self.viewModel = viewModel
+            self.emit?(viewModel)
         }
         
     }
