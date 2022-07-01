51116be002c166e479297a7961708b85e28baf51
matheus3006 <matheussouza18@live.com>
2022-07-01 16:21:17 -0300

    feat: add validation target

---
+++ swiftTDD/Validation/Validation.h
@@ -0,0 +1,18 @@
+//
+//  Validation.h
+//  Validation
+//
+//  Created by Matheus Souza on 7/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Validation.
+FOUNDATION_EXPORT double ValidationVersionNumber;
+
+//! Project version string for Validation.
+FOUNDATION_EXPORT const unsigned char ValidationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Validation/PublicHeader.h>
+
+
---
+++ swiftTDD/ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import Validation
+
+class EmailValidatorAdapterTests: XCTestCase {
+    func test_() throws {
+    
+    }
+
+}
