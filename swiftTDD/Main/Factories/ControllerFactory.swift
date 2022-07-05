e3745cf858aa7b9f0ff69110a069cc69387b0909
matheus3006 <matheussouza18@live.com>
2022-07-05 18:17:35 -0300

    feat: ensure SignUpPresenter has no memory leak with SignUpVIewController

--- swiftTDD/Main/Factories/ControllerFactory.swift
+++ swiftTDD/Main/Factories/ControllerFactory.swift
@@ -12,13 +12,31 @@ class ControllerFactory{
       let controller = SignUpViewController.instatiate()
         let emailValidatorAdapter = EmailValidatorAdapter()
         
-        let presenter = SignUpPresenter(alertView: controller, emailValidator: emailValidatorAdapter, addAccount: addAccount, loadingView: controller)
+        let presenter = SignUpPresenter(alertView: WeakVarProxy(controller), emailValidator: emailValidatorAdapter, addAccount: addAccount, loadingView: WeakVarProxy(controller))
         controller.signUp = presenter.signUp
 
         return controller
 
     }
+
+}
+
+class WeakVarProxy<T: AnyObject> {
+    private weak var instance: T?
     
-    
-    
+    init(_ instace: T){
+        self.instance = instace
+    }
+}
+
+extension WeakVarProxy: AlertView where T: AlertView {
+    func showMessage(viewModel: AlertViewModel) {
+        instance?.showMessage(viewModel: viewModel)
+    }
+}
+
+extension WeakVarProxy: LoadingView where T: LoadingView {
+    func display(viewModel: LoadingViewModel) {
+        instance?.display(viewModel: viewModel)
+    }
 }
---
+++ swiftTDD/MainTests/SignUpIntegrationTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+import Main
+
+class SignUpIntegrationTests: XCTestCase {
+
+    func test_ui_presentation_integration() throws {
+        let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
+        checkMemoryLeak(for: sut)
+    }
+}
